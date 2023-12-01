98e5d1cc5ca3bd563056d9fc909e865abca15d2b
Feyy <feyyshhd@gmail.com>
2023-12-01 09:30:14 +0800

    testing

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './Login';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center mt-5">FeyyFit</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
