9d07bd075e56e11d6fb3a5b483eda0dbe8315552
Feyy <feyyshhd@gmail.com>
2023-12-20 16:26:42 +0800

    fire

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VOTE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID
+    apiKey: "AIzaSyBjpNncU7UyjOi5dhtrWnWKHwmXaZNIMgI",
+    authDomain: "capstone-project-dc441.firebaseapp.com",
+    projectId: "capstone-project-dc441",
+    storageBucket: "capstone-project-dc441.appspot.com",
+    messagingSenderId: "146326925549",
+    appId: "1:146326925549:web:47a105585960d5845d9ce4"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const auth = getAuth(app);
+
